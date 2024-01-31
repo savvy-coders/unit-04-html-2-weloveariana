a84ae019f06f9d2e0d63f5c0a1172a85ad38e6b8
David Rodriguez <davidrodrigueznolimit@gmail.com>
2024-01-31 13:50:38 -0600

    update to remove cypress

--- cypress/e2e/html2Test5.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Form validation and submission page', () => {
-  beforeEach(() => {
-    cy.visit('/index.html');
-  });
-
-  it('has a submit button', () => {
-    cy.get('input[type=submit]').should('exist');
-  });
-});
